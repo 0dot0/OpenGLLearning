6e62c85f7c510cb25701254abfdea4ea78383617
hello <hello@gmail.com>
2022-08-22 07:53:59 +0800

    使用老式OpenGL画一个三角形

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -18,12 +18,18 @@ int main(void)
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
-
+    
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glBegin(GL_TRIANGLES);
+        glVertex2f(-0.5f, -0.5f);
+        glVertex2f(0.5f, -0.5f);
+        glVertex2f(0.0f, 0.5f);
+        glEnd();
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
