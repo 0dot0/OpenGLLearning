ba9eab922b0b61fb4cc4dcd2021c072dc7018417
hello <hello@gmail.com>
2022-09-02 07:29:41 +0800

    03-Hello Window 2

--- OpenGL/main.cpp
+++ OpenGL/main.cpp
@@ -1,10 +1,23 @@
-#define GLEW_STATIC
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
+#include <iostream>
+
+void ProcessInput(GLFWwindow* window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+    }
+}
+
 int main()
 {
-    glfwInit();
+    if(glfwInit() == GLFW_FALSE)
+    {
+        std::cout << "glfw init error." << std::endl;
+        return EXIT_FAILURE;
+    }
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
@@ -13,12 +26,20 @@ int main()
 
     glfwMakeContextCurrent(window);
 
-    glewInit();
+    glewExperimental = true;
+    if (glewInit() != GLEW_OK)
+    {
+        std::cout << "glew init error." << std::endl;
+        glfwTerminate();
+        return EXIT_FAILURE;
+    }
 
     glViewport(0, 0, 800, 600);
 
     while (!glfwWindowShouldClose(window))
     {
+        ProcessInput(window);
+
         glClearColor(1, 0, 0, 1);
         glClear(GL_COLOR_BUFFER_BIT);
 
@@ -27,6 +48,5 @@ int main()
     }
 
     glfwTerminate();
-
     return 0;
 }
